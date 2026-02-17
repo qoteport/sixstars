0952a6355daaf40b4e992fe0e78235ce0bbacf7b
QotePort <qoteport@gmail.com>
2026-02-17 13:52:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#fff" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={cn('font-body antialiased min-h-screen bg-background')}>
